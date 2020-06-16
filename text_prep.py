a71dd4f8050fb401045de5f076de22b4f6390b63
jazzij <jazzij@umich.edu>
2020-06-16 11:12:04 -0400

    first commit

---
+++ text_prep.py
@@ -0,0 +1,55 @@
+"""
+This script will convert rows of a CSV file into a series of text documents.
+The title of the document will be name of org, the text of the document taken from the text column.
+
+Following creation of the documents, 
+
+[ (name, textStr) ]
+
+"""
+from openpyxl import load_workbook
+import os
+csv_filename = "Sci_Tech_BLM_List_wText_Organizations.xlsx"
+#delimiter = '\t'
+#statements = []
+
+#create directory to store generated text files (if one does not already exist)
+curdir = os.getcwd()
+textdir = curdir + "/texts"
+if not os.path.isdir(textdir):
+	os.mkdir(textdir)
+
+#Get appropriate sheet from xcel file
+wb = load_workbook("Sci_Tech_BLM_List_wText.xlsx")
+statements = wb['Sheet6']
+
+# Save contexts of text into seperate txt files
+for row in statements.values:
+	name, text = row
+	if name is None:
+		continue
+	
+	#create new filename. make sure good punctuation	
+	name = name.replace('/', '_')
+	txtfilename = name + ".txt"		
+	path_to_file =os.path.join(textdir, txtfilename)
+	
+	#if file already exists, skip
+	if os.path.isfile(path_to_file):
+		continue
+	
+	#write the file
+	file = open( path_to_file, 'w')
+	if text:
+		file.write(text)
+	else:
+		print("Row {} returned bad value {}".format(name, text))
+	file.close()
+
+
+'''TODO:
+1) 
+2) clean headings from letter formatted text
+'''
+
+
