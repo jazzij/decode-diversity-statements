3ddac3ca79744d3a28bd1748d4a2997b3f9f91cd
kumbulat <kumbulat@berea.edu>
2020-07-25 15:16:57 -0400

    Work_in_progress.

--- ngrams.py
+++ ngrams.py
@@ -10,80 +10,58 @@ curdir = os.getcwd()
 textdir = (r"C:\Users\kumbulat\PycharmProjects\decode-diversity-statements\texts")
 newtextdir = (r"C:\Users\kumbulat\PycharmProjects\decode-diversity-statements")
 newfilePath = os.path.join(newtextdir, "common_ngrams.txt")
+
 text = []
 new_trigrams = []
 tokens = []
 ngram_counts ={}
 newWordsList = []
+def all_corpus_ngrams():
+    for filename in os.listdir(textdir):
+        filepath = os.path.join(textdir, filename)
+        with open(filepath, "r") as file:
+            text = file.read()
+            tokens = tokenizer.tokenize(text)
 
-for filename in os.listdir(textdir):
-    filepath = os.path.join(textdir, filename)
-    with open(filepath, "r") as file:
-        text = file.read()
-        tokens = tokenizer.tokenize(text)
+            for s in tokens:
+                if len(s) == 1 and s == "s":
+                    tokens.remove(s)
 
-        for s in tokens:
-            if len(s) == 1 and s == "s":
-                tokens.remove(s)
-        # print("TOKENS", tokens)
-        trigrams = Counter(ngrams(tokens, 3))
-        # print("This is the trigram",trigrams)
-        # for key,value in trigrams.items():
-        #     print(key)
-        ngram_counts.update(trigrams)
+            trigrams = Counter(ngrams(tokens, 3))  # return a dictionary, key is tuple, value is count
+            ngram_counts.update(trigrams)
+            all_texts = significant_ngrams(trigrams)
 
+            for i in trigrams:
+                newWordsList.append(i)
 
-        # print(" ===NGRAM COUNTS ===")
-        # print(ngram_counts)  # return a dictionary, key is tuple, value is count
+            grams = Counter(ngrams(newWordsList, 3))
+            common_counts = grams.most_common(150)
+    return common_counts
 
-        print(" === MOST COMMON ===")
-        most_common_counts = trigrams.most_common(10)
-        print(most_common_counts)
-        for i in most_common_counts:
-            for j in i[0]:
-                newWordsList.append(j)
-        grams = Counter(ngrams(newWordsList, 3))
-        common_counts = grams.most_common(30)
-print(common_counts)
-
-file = open("common_ngrams.txt", 'a+')
-file.write(str(common_counts))
-    # for j in i[0]:
-    #     file.write(j)
-    #     file.write(" ")
+def significant_ngrams(trigrams):
 
 
-
-# print("This is most NWL words",t)
-
-#         newStr += str(most_common_counts)
-#
-#         print("Most common counts",most_common_counts)  # return list of nested tuples [((a,b,c), #),]
-# revStr=""
-# unwanted = "[]{}�()"
-# for i in newStr:
-#     if i not in unwanted:
-#         revStr+=i
-#
-# file = open("common_ngrams.txt", 'w')
-# file.write(revStr)
+    tokenizer = nltk.RegexpTokenizer(r"\w+")
+    words = open("uniquewordslist.txt", 'r')
+    uniqueList = words.read()
+    token = tokenizer.tokenize(uniqueList)
 
 
-tokenizer = nltk.RegexpTokenizer(r"\w+")
-words = open("uniquewordslist.txt", 'r')
-uniqueList = words.read()
-token = tokenizer.tokenize(uniqueList)
-
-# print(" === NGRAM KEYS ===")
-ngram_keys = ngram_counts.keys()  # keys of the dictionary are the ngrams
-# print(ngram_keys)
+    ngram_keys = trigrams.keys()  # keys of the dictionary are the ngrams
+    # print(ngram_keys)
 
 
-for unique_word in token:
-    # find word in ngram
-    # print("***Searching for ", unique_word)
-    for ngram in ngram_keys:
-        if unique_word in ngram:
-            pass
-            # print(ngram, ngram_counts[ngram])
+    for unique_word in token:
+        # find word in ngram
+        # print("***Searching for ", unique_word)
+        for ngram in ngram_keys:
+            if unique_word in ngram:
+                significant = (ngram, ngram_counts[ngram])
+
+
+                file = open("common_ngrams.txt", 'a+')
+                file.write(str(significant))
+
+if __name__ == "__main__":
+    all_corpus_ngrams()
 
