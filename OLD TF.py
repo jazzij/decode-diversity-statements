b72833b6624c10d6d6e0db9c5dfe4a58e2908e0e
kumbulat <kumbulat@berea.edu>
2020-11-09 16:31:58 -0500

    Modifying term frequency

--- OLD TF.py
+++ OLD TF.py
@@ -9,6 +9,7 @@ from nltk import FreqDist
 import matplotlib.pyplot as plt
 
 filtered_words = []
+term_frequency = []
 curdir = os.getcwd()
 textdir = (r"C:\Users\kumbulat\PycharmProjects\decode-diversity-statements\texts")
 tokenizer = nltk.RegexpTokenizer(r"\w+")  # Removes all punctuation marks in the text
@@ -21,12 +22,14 @@ def stop_words(tokens):
     return: filtered_words
     """
 
-    stop_words = set(stopwords.words('english')) # Creating a List of stopwords
+    stop_words = set(stopwords.words('english'))  # Creating a List of stopwords
     for w in tokens:
         if w not in stop_words:
-            filtered_words.append(w)    # Add all words after stopwords have been removed
-    print ("Filtered_words:",filtered_words )
+            filtered_words.append(w)  # Add all words after stopwords have been removed
+    # print ("Filtered_words:",filtered_words )
     return filtered_words
+
+
 def termfrequency():
     """
     This function is supposed to calculate the term frequency of the text in file.
@@ -39,7 +42,16 @@ def termfrequency():
             text = file.read()
             tokens = tokenizer.tokenize(text)  # Returns a text as a list of words with punctuations removed.
             tokens = [token.lower() for token in tokens if token.isalpha()]  # Changes all the words into lower case
-            print("TOKENS:",tokens)
+            words = stop_words(tokens)
+            new_words = modified_files(words)
+
+            count = 0
+
+            for i in new_words:
+                count += 1
+                term_frequency.append((i, count / len(new_words)))
+            for i in term_frequency:
+                print(i)
 
     # with open(filename, "r") as file:
     #     text = file.read()
@@ -57,28 +69,32 @@ def termfrequency():
     #     term_frequency_graph = plot_tf(tf_grph)
     #     return myList
 
+
 def modified_files(filename):
     """
     This function is supposed to stem the words that end with -ing and lemmatize the rest. It is also supposed to remove duplicates of words.
     filename: Name of text files
     return: duplicates free
     """
+    global uniqueWords
     modified_words = []
     duplicates_free = []
-    with open(filename.txt, 'r') as file:
-        words = file.read()
-        new_words = word_tokenize(words)
+
     wn = nltk.WordNetLemmatizer()
     ps = PorterStemmer()
-    for w in new_words:
-        if w[-3:] == "ing": #Checks for the words that end with -ing and they going through stemming
+    for w in filename:
+        if w[-3:] == "ing":  # Checks for the words that end with -ing and they going through stemming
             modified_words.append(ps.stem(w))
         else:
             modified_words.append(wn.lemmatize(w))
-        uniqueWords = set(modified_words[:]) #This removes the duplicates of words after stemming and lemmatization
-        for i in uniqueWords:
-            duplicates_free.append(i)
+            uniqueWords = set(
+                modified_words[:])  # This removes the duplicates of words after stemming and lemmatization
+
+    for i in uniqueWords:
+        duplicates_free.append(i)
     return duplicates_free
+
+
 def plot_tf(filename):
     """
     This function is for plotting the term frequency of each file or allthewords list on a graph.
@@ -86,9 +102,10 @@ def plot_tf(filename):
     with open(filename.txt, 'r') as file:
         words = file.read()
         new_words = word_tokenize(words)
-        tf_grph = dict(new_words)  #Changes the files into a dictionary
+        tf_grph = dict(new_words)  # Changes the files into a dictionary
         fd = nltk.FreqDist(tf_grph)
-        fd.plot(40, cumulative=False)  #Plots forty terms with highest term frequency.
+        fd.plot(40, cumulative=False)  # Plots forty terms with highest term frequency.
+
 
 if __name__ == "__main__":
     termfrequency()
